06e1a89290c30c6c5146e684f4a0d935697d1241
tosqaaa <ilyarudakov.by@gmail.com>
2024-06-16 11:51:36 +0300

    Cleaned code

--- app/src/main/java/com/example/mypokemons/MainActivity.kt
+++ app/src/main/java/com/example/mypokemons/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.mypokemons
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.mypokemons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
