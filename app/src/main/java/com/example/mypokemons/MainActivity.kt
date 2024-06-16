48fe7be9d5f115826e2695f11dea6da4ac45e14a
tosqaaa <ilyarudakov.by@gmail.com>
2024-06-16 11:39:44 +0300

    Added detail activity

--- app/src/main/java/com/example/mypokemons/MainActivity.kt
+++ app/src/main/java/com/example/mypokemons/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.mypokemons
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mypokemons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pokemon1.setOnClickListener{
+            val intent = Intent(this, PokemonDetailActivity::class.java)
+            intent.putExtra("id", 1)
+            this.startActivity(intent)
+        }
+
+        binding.pokemon2.setOnClickListener{
+            val intent = Intent(this, PokemonDetailActivity::class.java)
+            intent.putExtra("id", 2)
+            this.startActivity(intent)
+        }
+
+        binding.pokemon3.setOnClickListener{
+            val intent = Intent(this, PokemonDetailActivity::class.java)
+            intent.putExtra("id", 3)
+            this.startActivity(intent)
+        }
+
+        binding.pokemon4.setOnClickListener{
+            val intent = Intent(this, PokemonDetailActivity::class.java)
+            intent.putExtra("id", 4)
+            this.startActivity(intent)
         }
     }
 }
