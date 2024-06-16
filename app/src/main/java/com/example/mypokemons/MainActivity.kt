afa2b6bfca77be6a8c5bbb27f563c43e7691492c
tosqaaa <ilyarudakov.by@gmail.com>
2024-06-16 18:25:28 +0300

    Deleted unnecessary lib, refactored duplicate code

--- app/src/main/java/com/example/mypokemons/MainActivity.kt
+++ app/src/main/java/com/example/mypokemons/MainActivity.kt
@@ -8,33 +8,31 @@ import com.example.mypokemons.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private fun startPokemonDetailActivity(pokemonId: Int) {
+        val intent = Intent(this, PokemonDetailActivity::class.java)
+        intent.putExtra("id", pokemonId)
+        startActivity(intent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.pokemon1.setOnClickListener{
-            val intent = Intent(this, PokemonDetailActivity::class.java)
-            intent.putExtra("id", 1)
-            this.startActivity(intent)
+        binding.pokemon1.setOnClickListener {
+            startPokemonDetailActivity(1)
         }
 
-        binding.pokemon2.setOnClickListener{
-            val intent = Intent(this, PokemonDetailActivity::class.java)
-            intent.putExtra("id", 2)
-            this.startActivity(intent)
+        binding.pokemon2.setOnClickListener {
+            startPokemonDetailActivity(2)
         }
 
-        binding.pokemon3.setOnClickListener{
-            val intent = Intent(this, PokemonDetailActivity::class.java)
-            intent.putExtra("id", 3)
-            this.startActivity(intent)
+        binding.pokemon3.setOnClickListener {
+            startPokemonDetailActivity(3)
         }
 
-        binding.pokemon4.setOnClickListener{
-            val intent = Intent(this, PokemonDetailActivity::class.java)
-            intent.putExtra("id", 4)
-            this.startActivity(intent)
+        binding.pokemon4.setOnClickListener {
+            startPokemonDetailActivity(4)
         }
     }
 }
